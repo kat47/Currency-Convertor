e1a3be8b4cfb81ed6fa86f7b3cb0972a757d7f3e
Ashish Kumar <ka_t47@yahoo.in>
2018-09-01 14:08:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ import requests
 import json
 def convert(conv):
     data = requests.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=INR&\
-    apikey=demo') #replace your api key with demo
+    apikey=demo') # change demo with api key
     #print(type(data.content))
     val = json.loads(data.content)
     #print(type(val))
     from_cur = val['Realtime Currency Exchange Rate']['1. From_Currency Code']
     to_cur = val['Realtime Currency Exchange Rate']['3. To_Currency Code']
     rate = float(val['Realtime Currency Exchange Rate']['5. Exchange Rate'])
-    print(f"{conv} {from_cur} = {conv*rate} {to_cur}")
+    final = conv*rate
+    conv = format(conv,'.2f')
+    final = format(final,'.2f')
+    print(f"{conv} {from_cur} = {final} {to_cur}")
 
 if __name__ == "__main__":
     conv = float(input('Enter a value to convert '))
     convert(conv)
+
